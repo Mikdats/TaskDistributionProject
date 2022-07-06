855532dc0a5f87f6cb7b8470b2878b5cbf0fd121
Mikdat Kerim GÜRSES <mikdatgurses61@gmail.com>
2022-07-06 19:08:06 +0300

    Add project files.

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class User:IEntity
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Kullanıcı adı kısmı gereklidir!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Şifre kısmı gereklidir!")]
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
